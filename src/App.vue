f55be1185a7e072ce57c4da6f623f84198aea75f
dom-elves <dominic.elves@netmatters-scs.com>
2022-04-26 11:25:44 +0100

    axios request for data response

--- src/App.vue
+++ src/App.vue
@@ -3,19 +3,46 @@
 
   <video-games></video-games>
   <contact-page></contact-page>
-  
+
 </div>
 </template>
 
 <script>
 import VideoGames from './components/VideoGames.vue'
 import ContactPage from './components/ContactPage.vue'
+import axios from 'axios'
 
 export default {
+
   name: 'App',
+
   components: {
+
     VideoGames,
     ContactPage
+
+  },
+
+  mounted() {
+
+    this.axiosRequest()
+
+  },
+
+  methods: {
+
+    axiosRequest() {
+      
+      axios.get('https://public.connectnow.org.uk/applicant-test/')
+
+        .then( function(response) {
+          console.log(response.data);
+        })
+
+        .catch( function(error) {
+          console.log(error);
+        })
+    }
   }
 }
 </script>

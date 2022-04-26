5dd5b175881f90b69b28aca982b8bae49ea46f6c
dom-elves <dominic.elves@netmatters-scs.com>
2022-04-26 16:05:37 +0100

    axios request now doesn't only return data in console but in html

--- src/components/VideoGames.vue
+++ src/components/VideoGames.vue
@@ -2,16 +2,48 @@
 
     <div>
 
-        
+        <p>{{ games }}</p>
 
     </div>
 
 </template>
 <script>
 
+import axios from 'axios'
+
+
 export default {
+
   name: 'VideoGames',
-  
+
+  data() {
+      return {
+          games: null,
+      }
+  },
+
+  mounted() {
+
+    this.axiosRequest()
+
+  },
+
+  methods: {
+
+    axiosRequest() {
+      
+      axios.get('https://public.connectnow.org.uk/applicant-test/')
+
+        .then(response => {
+            this.games = response
+        })
+
+        .catch(error => {
+          console.log(error);
+        })
+
+    }
+  }
 }
 
 </script>

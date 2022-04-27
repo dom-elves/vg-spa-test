8041886d86265eae783a03032be9d529c450e96a
dom-elves <dominic.elves@netmatters-scs.com>
2022-04-27 09:38:33 +0100

    improved date display and built basic filter with no functionality yet

--- src/components/VideoGames.vue
+++ src/components/VideoGames.vue
@@ -1,9 +1,34 @@
 <template>
 
-  <div>
+    <div>
+
+    <div class="flex flex-col bg-[#0e1a2b] p-3 mb-20">
+
+      <h2 class="text-white text-2xl mb-10">Filter Results</h2>
+
+      <div class="flex flex-col mb-3">
+        <label class="text-white mb-1">Name (contains)</label>
+        <input class="bg-[#182c47]">
+      </div>
+
+      <div class="flex flex-col mb-3">
+        <label class="text-white mb-1">Minimum score</label>
+        <input class="bg-[#182c47]">
+      </div>
+
+
+      <label class="text-white mb-1">Order By</label>
+      <select>
+        <option class="bg-[#182c47] text-white">Score</option>
+        <option class="bg-[#182c47] text-white">Name</option>
+      </select>
+
+    </div>
 
     <div v-if="this.loading == true">
+
       <p class="text-white">Loading...</p>
+
     </div>
 
     <div v-for="game in games" :key="game.id" class="bg-[#0e1a2b] mb-5"> 
@@ -66,8 +91,14 @@ export default {
             //parsing date to readable format
             let date = game.first_release_date;
             let jsDate = new Date(date);
-        
-            game.first_release_date = jsDate;
+
+            let yyyy = jsDate.getFullYear();
+            let mm = jsDate.getMonth() + 1;
+            let dd = jsDate.getDate();
+            
+            let displayedDate = dd + '/' + mm + '/' + yyyy
+
+            game.first_release_date = displayedDate;
 
             //rounding rating 
             let rating = game.rating;
